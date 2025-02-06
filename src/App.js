848c75f89ddec69da8eb752a2a9e089868b4e00a
Sarika-T <talarisarika22@gmail.com>
2025-02-06 17:55:49 +0530

    changes saved

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {BrowserRouter, HashRouter, Route, Routes} from "react-router-dom";
 import MainPage from './components/MainPage/MainPage';
 import { Provider } from 'react-redux';
 import store from './utils/DataStore';
@@ -12,7 +12,7 @@ function App() {
   return (
     <Provider store={store}>
       <AuthProvider>
-        <BrowserRouter>
+        <HashRouter>
           <div className='App'>
             <Routes>
               <Route path="/" element={<SignUp />} />
@@ -20,7 +20,7 @@ function App() {
               <Route path="/login" element={<SignIn />} />
             </Routes>
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </Provider>
   );
